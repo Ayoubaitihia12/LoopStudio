e6c0a8e28d76cebe57443e5d5812119cd5314591
Ayoubaitihia12 <87654009+Ayoubaitihia12@users.noreply.github.com>
2022-07-06 16:05:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//MOBILE NAV BAR
+const mobile_menu = document.querySelector(".mobile-menu");
+const nav_bar = document.querySelector(".nav-bar");
+
+mobile_menu.addEventListener("click",()=>{
+	mobile_menu.classList.toggle("menu-mobile-active")
+	nav_bar.classList.toggle("mobile-active");
+})
